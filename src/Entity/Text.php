70085f9a4693747b24428102e61fccf579b39d17
pimenvibritania <blackbrit13@gmail.com>
2019-01-22 01:17:01 +0700

    Add Getters and Setters

---
+++ src/Entity/Text.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TextRepository")
+ */
+class Text
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    //Getters and Setters
+    public function getId(){
+      return $this->id;
+    }
+
+    public function getTitle(){
+      return $this->title;
+    }
+
+    public function setTitle($title){
+      return $this->title = $title;
+    }
+
+    public function getBody(){
+      return $this->body;
+    }
+
+    public function setBody($body){
+      return $this->body = $body;
+    }
+
+
+
+    // public function getId(): ?int
+    // {
+    //     return $this->id;
+    // }
+
+
+}
