af012ccd90e564569c4d3fa6454663b393188381
Lucifer017 <irfandauss0@gmail.com>
2019-01-22 09:30:28 +0700

    Added form for new input

--- src/Controller/TextController.php
+++ src/Controller/TextController.php
@@ -2,10 +2,19 @@
   namespace App\Controller;
 
   use App\Entity\Text;
+
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\HttpFoundation\Request;
+
   use Symfony\Component\Routing\Annotation\Route;
+
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+  use Symfony\Component\Form\Extension\Core\Type\TextType;
+  use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+  use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
   class TextController extends Controller{
     /**
@@ -13,11 +22,37 @@
       *
       */
     public function index(){
-
       $texts= $this->getDoctrine()->getRepository(Text::class)->findAll();
+
       return $this->render('text/index.html.twig', array('texts' => $texts));
     }
 
+    /**
+      * @Route("/text/new", name="new_text")
+      * Method({"GET", "POST"})
+      */
+    public function new(Request $request){
+      $text = new Text();
+
+      $form = $this->createFormBuilder($text)
+      ->add('title', TextType::class, array('attr' =>
+      array('class' => 'form-control')))
+      ->add('body', TextareaType::class, array(
+        'required' => false,
+        'attr' => array('class' => 'form-control')
+      ))
+      ->add('save', SubmitType::class, array(
+        'label' => 'Create',
+        'attr' => array('class' => 'btn btn-primary mt-3')
+      ))
+      ->getForm();
+
+      return $this->render('text/new.html.twig', array(
+        'form' => $form->createView()
+      ));
+
+    }
+    
     /**
       * @Route("/text/{id}", name="text_show")
       *
@@ -27,6 +62,7 @@
 
       return $this->render('text/show.html.twig', array('text' => $text));
     }
+
     /**
       * @Route("/text/save")
       *
