8091543b27c41557c5f8a22cac0488fbac5ae43b
Lucifer017 <irfandauss0@gmail.com>
2019-01-22 08:39:25 +0700

    Added show function

--- src/Controller/TextController.php
+++ src/Controller/TextController.php
@@ -9,32 +9,41 @@
 
   class TextController extends Controller{
     /**
-      * @Route("/")
+      * @Route("/", name="text_list")
       *
       */
     public function index(){
-      //return new Response('<html><body>Test</body></html>');
-      $texts=['Text 1', 'Text 2'];
+
+      $texts= $this->getDoctrine()->getRepository(Text::class)->findAll();
       return $this->render('text/index.html.twig', array('texts' => $texts));
     }
 
     /**
+      * @Route("/text/{id}", name="text_show")
+      *
+      */
+    public function show($id){
+      $text = $this->getDoctrine()->getRepository(Text::class)->find($id);
+
+      return $this->render('text/show.html.twig', array('text' => $text));
+    }
+    /**
       * @Route("/text/save")
       *
       */
-      public function save(){
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $text = new Text();
-        $text->setTitle('Text Two');
-        $text->setBody('This is the body');
-
-        $entityManager->persist($text);
-
-        $entityManager->flush();
-
-        return new Response('Text saved with the id of '.$text->getId());
-      }
+      // public function save(){
+      //   $entityManager = $this->getDoctrine()->getManager();
+      //
+      //   $text = new Text();
+      //   $text->setTitle('Text Two');
+      //   $text->setBody('This is the body');
+      //
+      //   $entityManager->persist($text);
+      //
+      //   $entityManager->flush();
+      //
+      //   return new Response('Text saved with the id of '.$text->getId());
+      // }
 
   }
 
